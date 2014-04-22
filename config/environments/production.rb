343c96359824fcdccb66ebd082b55236d65b6124
wmsuke <wmsuke@gmail.com>
2014-04-22 18:20:53 +0900

    本番設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
-Rails.application.configure do
+FirstApp::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -79,5 +80,5 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  #config.active_record.dump_schema_after_migration = false
 end
