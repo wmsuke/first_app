03cc8a400d43abdd2ad6fc1a81fac6d9049ed563
wmsuke <wmsuke@gmail.com>
2014-04-22 19:16:56 +0900

    タイトルをレイアウトで共通化する

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,14 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'First App'" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('First App')
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -16,6 +21,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
@@ -23,6 +33,11 @@ describe "Static pages" do
     it "should have the content 'About Us'" do 
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
